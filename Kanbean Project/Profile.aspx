907c7f509e62c66d34b76c18a4844bdd80b14005
NghiemTrung <nghiem.duc.trung@gmail.com>
2014-12-09 00:34:34 +0200

    Show profile

--- Kanbean Project/Profile.aspx
+++ Kanbean Project/Profile.aspx
@@ -5,10 +5,82 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <link rel="stylesheet" href="/css/style.css" type="text/css" />
+    <style type="text/css">
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
+        <div id="user-information-profile">
+            <p>Project:
+                <asp:DropDownList ID="projectDropDownList" runat="server" AutoPostBack="true" OnSelectedIndexChanged="projectDropDownList_SelectedIndexChanged"></asp:DropDownList>
+                Welcome,
+                <asp:LinkButton ID="linkBtnUsername" runat="server" OnClick="linkBtnUsername_Click" Text="Username"></asp:LinkButton>. (<asp:LinkButton ID="linkBtnLogout" runat="server" OnClick="EatCookies">Logout</asp:LinkButton>)
+            </p>
+        </div>
     <div>
+        <div style="background-color: deepskyblue"><h1 class="formTitle-profile">Kanbean board</h1></div>
+        <table style="width:500px; margin:0 auto">
+            <tr>
+                <td style ="width:200px">Username</td>
+                <td>
+                    <asp:TextBox ID="TxtUsername" runat="server" CssClass="TextBox"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>
+                    <asp:TextBox ID="TxtPassword" runat="server" CssClass="TextBox"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>
+                    <asp:TextBox ID="TxtEmail" runat="server" TextMode="Email" CssClass="TextBox"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>User level</td>
+                <td>
+                    <asp:TextBox ID="TextLevel" runat="server" CssClass="TextBox"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>Participated Project(s)</td>
+                <td>
+                    <asp:DropDownList ID="DropProjects" runat="server" CssClass="TextBox">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <asp:LinkButton ID="SetDefault" runat="server" OnClick="SetDefaulProject">Set as Default Project</asp:LinkButton>
+                </td>
+            </tr>
+            <tr>
+                <td>Default Project</td>
+                <td>
+                    <asp:TextBox ID="TextDefaultProject" runat="server" CssClass="TextBox"></asp:TextBox>
+                </td>
+            </tr>
+        </table>
     
     </div>
     </form>
--- Kanbean Project/Profile.aspx.cs
+++ Kanbean Project/Profile.aspx.cs
@@ -4,12 +4,139 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
 
 namespace Kanbean_Project
 {
     public partial class Profile : System.Web.UI.Page
     {
+        OleDbConnection myConnection = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|LanbanDatabase.mdb;");
+        OleDbCommand mySelectCommand = new OleDbCommand();
+        OleDbCommand myUpdateCommand = new OleDbCommand();
+        OleDbCommand myDeleteCommand = new OleDbCommand();
+        OleDbDataAdapter myAdapter = new OleDbDataAdapter();
+        DataSet myDataSet = new DataSet();
+        private void getDB()
+        {
+            myConnection.Open();
+            mySelectCommand.Connection = myConnection;
+            mySelectCommand.CommandType = CommandType.Text;
+            myAdapter.SelectCommand = mySelectCommand;
+            mySelectCommand.CommandText = "SELECT Projects.ProjectID, Projects.ProjectName, User.* "
+                                        + "FROM [User], Projects "
+                                        + "WHERE (((User.UserID)=" + Request.QueryString["UserID"] + ") AND "
+                                        +   "((Projects.ProjectID)=[User].[DefaultProjectID]));";
+            myAdapter.Fill(myDataSet, "User");
+            mySelectCommand.CommandText = "SELECT Tasks.TaskTitle, Tasks.TaskComplexity, Tasks.TaskStatusID, Tasks.TaskEstimationHour, "
+                                        +   "Tasks.TaskSpentTime, Tasks.TaskDueDate, Projects.ProjectID, Tasks.TaskID, Projects.ProjectName "
+                                        + "FROM [User], Projects, ProjectsMembers, Backlogs, Tasks "
+                                        + "WHERE (((Tasks.BacklogID)=[Backlogs].[BacklogID]) AND "
+                                        +   "((Projects.ProjectID)=[Backlogs].[ProjectID]) AND "
+                                        +   "((ProjectsMembers.UserID)=[User].[UserID]) AND "
+                                        +   "((ProjectsMembers.ProjectID)=[Projects].[ProjectID]) AND "
+                                        +   "((User.UserID)=" + Request.QueryString["UserID"] + ") AND "
+                                        +   "((User.UserID)=[Tasks].[TaskAssigneeID])) "
+                                        + "ORDER BY Tasks.TaskID;";
+            myAdapter.Fill(myDataSet, "Task");
+            mySelectCommand.CommandText = "SELECT User.UserID, Projects.* "
+                                        + "FROM [User], Projects, ProjectsMembers "
+                                        + "WHERE (((User.UserID)=" + Request.QueryString["UserID"] + ") AND "
+                                        +   "((ProjectsMembers.ProjectID)=[Projects].[ProjectID]) AND "
+                                        +   "((ProjectsMembers.UserID)=[user].[UserID])) "
+                                        + "ORDER BY ProjectsMembers.ProjectID;";
+            myAdapter.Fill(myDataSet, "Projects");
+            mySelectCommand.CommandText = "SELECT * FROM Projects ORDER BY ProjectID;";
+            myAdapter.Fill(myDataSet, "RawProjects");
+            myConnection.Close();
+        }
         protected void Page_Load(object sender, EventArgs e)
+        {
+            //reading cookies
+            string Username = "";
+            if (Request.Cookies["UserSettings"] != null)
+            {
+                if (Request.Cookies["UserSettings"]["Name"] != null)
+                {
+                    Username = Request.Cookies["UserSettings"]["Name"];
+                    linkBtnUsername.Text = Username;
+                }
+                else Response.Redirect("login.aspx");
+            }
+            else Response.Redirect("login.aspx");
+            FillContent();
+            //fill project drop down list
+            foreach (DataRow row in myDataSet.Tables["RawProjects"].Rows)
+            {
+                projectDropDownList.Items.Add(row["ProjectName"].ToString());
+                projectDropDownList.Items[projectDropDownList.Items.Count - 1].Value = row["ProjectID"].ToString();
+            }
+        }
+
+        private void FillContent()
+        {
+            //get database base on UserID
+            getDB();
+            //fill content
+            foreach (DataRow row in myDataSet.Tables["User"].Rows)
+            {
+                TxtUsername.Text = row["Username"].ToString(); TxtUsername.Enabled = false;
+                string password = row["Password"].ToString();
+                foreach (char c in password)
+                {
+                    if (password.IndexOf(c) != 0 && password.IndexOf(c) != (password.Length - 1))
+                    {
+                        char[] Char = password.ToCharArray();
+                        Char[password.IndexOf(c)] = '*';
+                        password = new string(Char);
+                    }
+                } TxtPassword.Text = password; TxtPassword.Enabled = false;
+
+                if (row["Email"].ToString() == "" || row["Email"].ToString() == null)
+                {
+                    TxtEmail.Text = "Unknown";
+                }
+                else
+                {
+                    TxtEmail.Text = row["Email"].ToString();
+                } TxtEmail.Enabled = false;
+
+                if (row["Level"].ToString() == "1") { TextLevel.Text = "admin"; }
+                else { TextLevel.Text = "User"; } TextLevel.Enabled = false;
+
+                TextDefaultProject.Text = row["ProjectName"].ToString(); TextDefaultProject.Enabled = false;
+
+                foreach (DataRow row1 in myDataSet.Tables["Projects"].Rows)
+                {
+                    DropProjects.Items.Add(row1["ProjectName"].ToString());
+                    DropProjects.Items[DropProjects.Items.Count - 1].Value = row1["ProjectID"].ToString();
+                }
+            }
+        }
+        protected void linkBtnUsername_Click(object sender, EventArgs e)
+        {
+            myConnection.Close();
+            Response.Redirect("Profile.aspx?userID=" + Session["userID"].ToString());
+        }
+
+        protected void EatCookies(object sender, EventArgs e)
+        {
+            if (Request.Cookies["UserSettings"] != null)
+            {
+                Response.Cookies["UserSettings"].Expires = DateTime.Now.AddDays(-1);
+                myConnection.Close();
+                Response.Redirect("login.aspx");
+            }
+        }
+
+
+        protected void projectDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            myConnection.Close();
+            Response.Redirect("board.aspx?projectID=" + projectDropDownList.SelectedValue);
+        }
+
+        protected void SetDefaulProject(object sender, EventArgs e)
         {
 
         }
